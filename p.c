ec6bbacd53527d4db064e561fa3b9b531ef4d3de
Akash Dwivedi <akashdwivedi56789@gmail.com>
2026-07-08 12:51:07 +0530

    1D Array

--- p.c
+++ p.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 int main() {
-    printf("\n--DSA--\n");
-    printf("Akash Dwivedi");
+    int arr[3];
+    for (int i = 0; i < 3; i++)
+    {
+        printf("Enter element at index %d : " , i);
+        scanf("%d", &arr[i]);
+    }
+
+    printf("\nArray Elements : ");
+    for (int i = 0; i < 3; i++)
+    {
+        printf("%d " , arr[i]); }
+    
     return 0;
 }
