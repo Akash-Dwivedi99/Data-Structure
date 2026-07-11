be4266d5c62835f20f95b42d4d51966d06e5eb7f
Akash Dwivedi <akashdwivedi56789@gmail.com>
2026-07-11 23:11:27 +0530

    deletion and reverse

--- CLL.c
+++ CLL.c
@@ -8,10 +8,10 @@ int main () {
     struct node *next;
 
 }; 
-   struct node *head, *temp, *newnode; /*end*/
+   struct node *head, *temp, *newnode, *end ;
 
     head = 0;
-    int choice = 1;
+    int choice = 1, ch;
 
     while(choice) {
         newnode = (struct node *) malloc(sizeof(struct node));
@@ -32,9 +32,8 @@ int main () {
     }
 
     if (head != NULL){
-        //end = temp;          
-        temp->next=head;
-        //end->next = head;
+        end = temp;          //temp->next=head;
+        end->next = head;
     }
 
 
@@ -45,7 +44,79 @@ int main () {
         temp = temp -> next;
     }
     
-    printf("%d", temp->data);
+    printf("%d -> ", end->data);
+
+    printf("\nDo you want to delete node from the begining (1 for begining , 2 for end, 3 to reverse list)? : ");
+    scanf("%d", &ch);
+
+    if(ch==1){
+        temp = head;
+        head = head->next;
+        end->next = head;
+        free(temp);
+
+        printf("\nAfter deletion : Linked List\n");
+        temp = head;
+        while(temp->next != head){
+           printf("%d -> ", temp->data);
+           temp = temp -> next;
+        }
+        printf("%d", temp->data);
+    }
+
+    else if(ch==2){
+        if(head==NULL){
+            printf("\nList is empty\n");
+        } 
+        else{
+
+            temp = head;
+            while(temp->next != head)
+            {
+            end = temp;
+            temp = temp->next;
+            }
+
+            end->next = head;
+            free(temp);
+
+        printf("\nAfter deletion : Linked List\n");
+        temp = head;
+        while(temp->next != head){
+           printf("%d -> ", temp->data);
+           temp = temp -> next;
+        }
+        printf("%d", temp->data);
+
+    }}
+    else{
+        struct node *prev, *current, *nextnode;
+
+prev = end;
+current = head;
+
+do
+{
+    nextnode = current->next;
+    current->next = prev;
+
+    prev = current;
+    current = nextnode;
+
+} while(current != head);
+
+head = prev;
+
+        printf("\nAfter reversing : Linked List\n");
+        temp = head;
+        while(temp->next != head){
+           printf("%d -> ", temp->data);
+           temp = temp -> next;
+        }
+        printf("%d", temp->data);
+
+    }
     return 0;
 
+    
 }
